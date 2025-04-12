4a4ce70995849773292009d1832a2c9c6a23481b
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-04-12 15:48:15 +0400

    feat: configure colors and their path

---
+++ colors.ts
@@ -0,0 +1,17 @@
+export const color = {
+    primaryColor: "#00ff00",
+    solidColor: "#00ff0059",
+    secondaryColor: "#f5f5f5",
+    tertiaryColor: "#555755",
+    
+    // <----- ----->
+    senderBgColor: "#00ff00",
+    receiverBgColor: "#f5f5f5",
+    chatRoomBgColor: "#00ff00",
+    textColor: "#000000",
+    
+    // <----- ----->
+    inputBgColor: "#f7f7f7",
+    inputBorderColor: "#ddd",
+    inputTextColor: "#333",
+}
