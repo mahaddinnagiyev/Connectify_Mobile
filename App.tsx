f5d13ae453d6e183e58e2b3d288c3cdaaa97f8de
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-04-17 17:29:19 +0400

    feat: setting environment variables setup

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "@navigation/Navigator";
 import { Provider } from "react-redux";
-import { store } from "./src/redux/store";
+import { store } from "@redux/store";
 
 export default function App() {
   return (
