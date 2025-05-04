ad30b5840fe785ce18b5655ad96fb3e80188b930
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-05-04 14:50:35 +0400

    feat: create new friend request received notifications in chat header

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, Pressable, ActivityIndicator } from "react-native";
 import { styles } from "./styles/chatHeader.style";
 import { MaterialIcons } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
 
 // Navigation
 import type { StackParamList } from "@navigation/UserStack";
@@ -14,69 +15,154 @@ import { useSocketContext } from "@context/SocketContext";
 // Functions
 import { truncate } from "@functions/messages.function";
 
+// Redux
+import { RootState } from "@redux/store";
+import { useSelector } from "react-redux";
+
+// Hooks
+import { useFriendData } from "@hooks/useFriendData";
+
+// Enums
+import { FriendshipAction } from "@enums/friendship.enum";
+
 const ChatHeader = () => {
-  // Functions
   const { navigate, goBack } =
     useNavigation<NativeStackNavigationProp<StackParamList>>();
   const route = useRoute<RouteProp<StackParamList, "Chat">>();
   const { chat } = route.params;
 
   const socket = useSocketContext();
+  const { acceptAndRejectFrienship, isAccepting, isRejecting } =
+    useFriendData();
 
   const handleBack = () => {
     socket?.emit("leaveRoom", { roomId: chat.id });
     goBack();
   };
 
+  const { receivedFriendshipRequests = [] } = useSelector(
+    (state: RootState) => state.myFriends
+  );
+
+  const [isFriendRequestReceived, setIsFriendRequestReceived] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    const isFriendRequestReceived = receivedFriendshipRequests.some(
+      (friend) => friend.requester.id === chat.otherUser.id
+    );
+    setIsFriendRequestReceived(isFriendRequestReceived);
+  }, [receivedFriendshipRequests]);
+
+  const handleAcceptAndRejectFriendship = async (action: FriendshipAction) => {
+    const friendRequest = receivedFriendshipRequests.find(
+      (request) => request.requester.id === chat.otherUser.id
+    );
+
+    await acceptAndRejectFrienship(action, friendRequest!.id);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.leftHeader}>
-        {/* Back Icon */}
-        <View>
-          <Pressable onPress={handleBack}>
-            <MaterialIcons name="arrow-back-ios" size={18} color="black" />
+    <>
+      <View style={styles.container}>
+        <View style={styles.leftHeader}>
+          {/* Back Icon */}
+          <View>
+            <Pressable onPress={handleBack}>
+              <MaterialIcons name="arrow-back-ios" size={18} color="black" />
+            </Pressable>
+          </View>
+
+          {/* User Details */}
+          <Pressable
+            style={styles.userDetail}
+            onPress={() => navigate("ChatDetail", { chat: chat })}
+          >
+            <Image
+              source={
+                chat.otherUserAccount.profile_picture
+                  ? { uri: chat.otherUserAccount.profile_picture }
+                  : require("@assets/images/no-profile-photo.png")
+              }
+              style={styles.profilePhoto}
+            />
+
+            <View style={{ gap: 5 }}>
+              <Text style={styles.roomName}>
+                {chat.name
+                  ? truncate(chat.name, 30)
+                  : truncate(
+                      `${chat.otherUser.first_name} ${chat.otherUser.last_name} | @${chat.otherUser.username}`,
+                      30
+                    )}
+              </Text>
+              <Text style={styles.lastSeen}>
+                Last Seen:{" "}
+                {new Date(
+                  chat.otherUserAccount.last_login! + "Z"
+                ).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                })}
+              </Text>
+            </View>
           </Pressable>
         </View>
-
-        {/* User Details */}
-        <Pressable
-          style={styles.userDetail}
-          onPress={() => navigate("ChatDetail", { chat: chat })}
-        >
-          <Image
-            source={
-              chat.otherUserAccount.profile_picture
-                ? { uri: chat.otherUserAccount.profile_picture }
-                : require("@assets/images/no-profile-photo.png")
-            }
-            style={styles.profilePhoto}
-          />
-
-          <View style={{ gap: 5 }}>
-            <Text style={styles.roomName}>
-              {chat.name
-                ? truncate(chat.name, 30)
-                : truncate(
-                    `${chat.otherUser.first_name} ${chat.otherUser.last_name} | @${chat.otherUser.username}`,
-                    30
-                  )}
-            </Text>
-            <Text style={styles.lastSeen}>
-              Last Seen:{" "}
-              {new Date(
-                chat.otherUserAccount.last_login! + "Z"
-              ).toLocaleDateString("en-US", {
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-              })}
-            </Text>
-          </View>
-        </Pressable>
       </View>
-    </View>
+
+      {isFriendRequestReceived && (
+        <View style={styles.notificationWrapper}>
+          <BlurView intensity={30} tint="light" style={styles.blurContainer}>
+            <View style={styles.notificationContent}>
+              <Text style={styles.notificationText}>
+                {`${chat.otherUser.first_name} ${chat.otherUser.last_name} sent you a friend request.`}
+              </Text>
+
+              <View style={styles.actionsContainer}>
+                <Pressable
+                  style={[styles.iconButton, styles.acceptButton]}
+                  onPress={() => {
+                    handleAcceptAndRejectFriendship(FriendshipAction.accept);
+                  }}
+                  disabled={isAccepting || isRejecting}
+                >
+                  {isAccepting ? (
+                    <ActivityIndicator size={"small"} color={"white"} />
+                  ) : (
+                    <Text style={styles.btnText}>Accept</Text>
+                  )}
+                </Pressable>
+
+                <Pressable
+                  style={[styles.iconButton, styles.rejectButton]}
+                  onPress={() => {
+                    handleAcceptAndRejectFriendship(FriendshipAction.reject);
+                  }}
+                  disabled={isAccepting || isRejecting}
+                >
+                  {isRejecting ? (
+                    <ActivityIndicator size={"small"} color={"white"} />
+                  ) : (
+                    <Text style={styles.btnText}>Reject</Text>
+                  )}
+                </Pressable>
+
+                <Pressable
+                  style={[styles.iconButton, styles.closeButton]}
+                  onPress={() => setIsFriendRequestReceived(false)}
+                  disabled={isAccepting || isRejecting}
+                >
+                  <Text style={styles.btnText}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+          </BlurView>
+        </View>
+      )}
+    </>
   );
 };
 
--- src/components/chat/styles/chatHeader.style.ts
+++ src/components/chat/styles/chatHeader.style.ts
@@ -44,4 +44,58 @@ export const styles = StyleSheet.create({
   lastSeen: {
     fontSize: 11,
   },
+  notificationWrapper: {
+    position: "absolute",
+    top: 120,
+    left: 0,
+    right: 0,
+    zIndex: 999,
+  },
+  blurContainer: {
+    padding: 16,
+    backgroundColor: "rgba(255,255,255,0.3)",
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.1)",
+  },
+  notificationContent: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 60,
+  },
+  notificationText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#2d3436",
+    marginRight: 10,
+    flexShrink: 1,
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  iconButton: {
+    width: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "rgba(255,255,255,0.3)",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+  },
+  btnText: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "#fff",
+  },
+  acceptButton: {
+    backgroundColor: "rgba(46, 213, 115, 0.9)",
+  },
+  rejectButton: {
+    backgroundColor: "rgba(255, 71, 87, 0.9)",
+  },
+  closeButton: {
+    backgroundColor: "rgba(178, 190, 195, 0.9)",
+  },
 });
