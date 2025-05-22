2c262c1a73c0ecb007bbddc206eac53dcd007716
Nagiyev Mahaddin <nmeheddin9@gmail.com>
2025-05-22 23:10:20 +0400

    feat: add new theme category and themes

--- themes.ts
+++ themes.ts
@@ -1,6 +1,7 @@
 export const backgroundThemes = {
   default: require("@assets/background/image10.jpg"),
 
+  // City
   "city.bg_1": require("@assets/background/image9.jpg"),
   "city.bg_2": require("@assets/background/image11.jpg"),
   "city.bg_3": require("@assets/background/image17.jpg"),
@@ -8,6 +9,7 @@ export const backgroundThemes = {
   "city.bg_5": require("@assets/background/image19.jpg"),
   "city.bg_6": require("@assets/background/image20.jpg"),
 
+  // Sky
   "sky.bg_1": require("@assets/background/image4.jpg"),
   "sky.bg_2": require("@assets/background/image5.jpg"),
   "sky.bg_3": require("@assets/background/image6.jpg"),
@@ -16,10 +18,42 @@ export const backgroundThemes = {
   "sky.bg_6": require("@assets/background/image12.jpg"),
   "sky.bg_7": require("@assets/background/image13.jpg"),
 
+  // Simple Color And Design
   "simple_color_and_design.bg_1": require("@assets/background/image1.jpeg"),
   "simple_color_and_design.bg_2": require("@assets/background/image2.jpg"),
   "simple_color_and_design.bg_3": require("@assets/background/image3.jpg"),
   "simple_color_and_design.bg_4": require("@assets/background/image14.jpg"),
   "simple_color_and_design.bg_5": require("@assets/background/image15.jpg"),
   "simple_color_and_design.bg_6": require("@assets/background/image16.jpg"),
+
+  // Games
+  "games.bg_1": require("@assets/background/image21.jpg"),
+  "games.bg_2": require("@assets/background/image22.jpg"),
+  "games.bg_3": require("@assets/background/image23.jpg"),
+  "games.bg_4": require("@assets/background/image24.jpg"),
+  "games.bg_5": require("@assets/background/image25.jpg"),
+  "games.bg_6": require("@assets/background/image26.jpg"),
+  "games.bg_7": require("@assets/background/image27.jpg"),
+  "games.bg_8": require("@assets/background/image28.jpg"),
+  "games.bg_9": require("@assets/background/image29.jpg"),
+  "games.bg_10": require("@assets/background/image30.jpg"),
+  "games.bg_11": require("@assets/background/image31.jpg"),
+  "games.bg_12": require("@assets/background/image32.jpg"),
+  "games.bg_13": require("@assets/background/image33.jpg"),
+  "games.bg_14": require("@assets/background/image34.jpg"),
+  "games.bg_15": require("@assets/background/image35.jpg"),
+  "games.bg_16": require("@assets/background/image36.png"),
+
+  "games.bg_17": require("@assets/background/image37.jpg"),
+  "games.bg_18": require("@assets/background/image38.jpg"),
+  "games.bg_19": require("@assets/background/image39.jpg"),
+  "games.bg_20": require("@assets/background/image40.jpg"),
+  "games.bg_21": require("@assets/background/image41.jpg"),
+  "games.bg_22": require("@assets/background/image42.jpg"),
+  "games.bg_23": require("@assets/background/image43.jpg"),
+  "games.bg_24": require("@assets/background/image44.jpg"),
+  "games.bg_25": require("@assets/background/image45.jpg"),
+  "games.bg_26": require("@assets/background/image46.jpg"),
+  "games.bg_27": require("@assets/background/image47.jpg"),
+  "games.bg_28": require("@assets/background/image48.jpg"),
 };
